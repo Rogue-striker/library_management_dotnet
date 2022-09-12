1aef9aa839e900b138dab43d89f50006a0351a12
kiran peraka <jyothikiran52@gmail.com>
2022-09-12 11:58:16 +0530

    changed database connection string

--- services/DatabaseService.cs
+++ services/DatabaseService.cs
@@ -5,7 +5,7 @@ public class DatabaseService : IDatabaseService{
     public SqlConnection getConnection(){
        // this.con = new SqlConnection("Server=127.0.0.1,1433;Database=library;User Id=SA;Password=Kiran@123!;");
        //this.con = new SqlConnection(@"Server=localhost\SQLEXPRESS;Database=librarydb;Trusted_Connection=true");
-      / this.con = new SqlConnection ("Server=tcp:databaselibrary.database.windows.net,1433;Initial Catalog=libraryDB;Persist Security Info=False;User ID=kiranperaka;Password=Kiran@123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+       this.con = new SqlConnection ("Server=tcp:databaselibrary.database.windows.net,1433;Initial Catalog=libraryDB;Persist Security Info=False;User ID=kiranperaka;Password=Kiran@123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
         return con;
     }
 
