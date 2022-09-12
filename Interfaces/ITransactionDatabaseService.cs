4da43a4c3d0055bfaaeb908d3870c5ec767c4fd6
kiran peraka <jyothikiran52@gmail.com>
2022-09-12 17:01:40 +0530

    removed transactionDatabaseService

--- Interfaces/ITransactionDatabaseService.cs
+++
@@ -1,5 +0,0 @@
-using System.Data.SqlClient;
-public interface ITransactionDatabaseService {
-    List<BookTransactionsModel> getUserTransactions(int userId);
-    List<BookTransactionsModel> getAllTransactions();
-}
